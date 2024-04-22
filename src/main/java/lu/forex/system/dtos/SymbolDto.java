e993b5b75770b8142027dfed127bdc90cd71ffcc
Allan de Miranda Silva <allandemiranda@gmail.com>
2024-04-22 08:53:01 +0200

    init

--- src/main/java/lu/forex/system/dtos/SymbolDto.java
+++ src/main/java/lu/forex/system/dtos/SymbolDto.java
@@ -4,9 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.NonNull;
 import lombok.experimental.Accessors;
 import lu.forex.system.enums.Currency;
-import lu.forex.system.model.Swap;
 
 @Accessors(fluent = true)
-public record SymbolDto(@NonNull @NotBlank String value, @NonNull Currency margin, @NonNull Currency profit, int digits, @NonNull Swap swap) {
+public record SymbolDto(@NonNull @NotBlank String value, @NonNull Currency margin, @NonNull Currency profit, int digits, @NonNull SwapDto swap) {
 
 }
