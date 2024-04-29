71a39e5e64b8e3dcc7844ec85cf789225c639a49
Allan de Miranda Silva <allandemiranda@gmail.com>
2024-04-29 11:23:28 +0200

    b1

--- src/main/java/lu/forex/system/services/SymbolService.java
+++ src/main/java/lu/forex/system/services/SymbolService.java
@@ -3,57 +3,28 @@ package lu.forex.system.services;
 import jakarta.validation.constraints.NotNull;
 import java.util.Collection;
 import java.util.Optional;
-import lombok.AccessLevel;
-import lombok.Getter;
 import lu.forex.system.dtos.SymbolCreateDto;
 import lu.forex.system.dtos.SymbolResponseDto;
 import lu.forex.system.dtos.SymbolUpdateDto;
-import lu.forex.system.entities.Symbol;
-import lu.forex.system.exceptions.SymbolNotFoundException;
-import lu.forex.system.mappers.SymbolMapper;
-import lu.forex.system.repositories.SymbolRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Getter(AccessLevel.PRIVATE)
-public class SymbolService {
+public interface SymbolService {
 
-  private final SymbolRepository symbolRepository;
-  private final SymbolMapper symbolMapper;
+  @NotNull
+  Collection<@NotNull SymbolResponseDto> getSymbols();
 
-  @Autowired
-  public SymbolService(final SymbolRepository symbolRepository, final SymbolMapper symbolMapper) {
-    this.symbolRepository = symbolRepository;
-    this.symbolMapper = symbolMapper;
-  }
+  @NotNull
+  Optional<@NotNull SymbolResponseDto> getSymbol(@NotNull String name);
 
-  public @NotNull Collection<@NotNull SymbolResponseDto> getSymbols() {
-    return this.getSymbolRepository().findAll().stream().map(symbolMapper::toDto).toList();
-  }
-
-  public @NotNull Optional<@NotNull SymbolResponseDto> getSymbol(final @NotNull String name) {
-    return this.getSymbolRepository().findFirstByNameOrderByNameAsc(name).map(symbolMapper::toDto);
-  }
-
-  public @NotNull SymbolResponseDto addSymbol(final @NotNull SymbolCreateDto symbolCreateDto) {
-    final Symbol symbol = symbolMapper.toEntity(symbolCreateDto);
-    final Symbol saved = this.getSymbolRepository().save(symbol);
-    return symbolMapper.toDto(saved);
-  }
+  @NotNull
+  SymbolResponseDto addSymbol(@NotNull SymbolCreateDto symbolCreateDto);
 
   @Transactional
-  public @NotNull SymbolResponseDto updateSymbol(final @NotNull SymbolUpdateDto symbolUpdateDto, final @NotNull String name) {
-    final Symbol symbol = this.getSymbolRepository().findFirstByNameOrderByNameAsc(name).orElseThrow(SymbolNotFoundException::new);
-    symbol.setDigits(symbolUpdateDto.digits());
-    symbol.setSwapShort(symbolUpdateDto.swapShort());
-    symbol.setSwapLong(symbolUpdateDto.swapLong());
-    return symbolMapper.toDto(this.getSymbolRepository().save(symbol));
-  }
+  @NotNull
+  SymbolResponseDto updateSymbol(@NotNull SymbolUpdateDto symbolUpdateDto, @NotNull String name);
 
   @Transactional
-  public boolean deleteSymbol(final @NotNull String name) {
-    return this.getSymbolRepository().deleteByName(name) > 0L;
-  }
+  boolean deleteSymbol(@NotNull String name);
 }
--- src/main/java/lu/forex/system/services/TickService.java
+++ src/main/java/lu/forex/system/services/TickService.java
@@ -3,44 +3,16 @@ package lu.forex.system.services;
 import jakarta.annotation.Nonnull;
 import jakarta.validation.constraints.NotNull;
 import java.util.Collection;
-import lombok.AccessLevel;
-import lombok.Getter;
 import lu.forex.system.dtos.TickCreateDto;
 import lu.forex.system.dtos.TickResponseDto;
-import lu.forex.system.entities.Symbol;
-import lu.forex.system.entities.Tick;
-import lu.forex.system.exceptions.SymbolNotFoundException;
-import lu.forex.system.mappers.TickMapper;
-import lu.forex.system.repositories.SymbolRepository;
-import lu.forex.system.repositories.TickRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@Getter(AccessLevel.PRIVATE)
-public class TickService {
+public interface TickService {
 
-  private final TickRepository tickRepository;
-  private final SymbolRepository symbolRepository;
-  private final TickMapper tickMapper;
+  @Nonnull
+  Collection<@NotNull TickResponseDto> getTicks(@Nonnull String symbolName);
 
-  @Autowired
-  public TickService(final TickRepository tickRepository, final SymbolRepository symbolRepository, final TickMapper tickMapper) {
-    this.tickRepository = tickRepository;
-    this.symbolRepository = symbolRepository;
-    this.tickMapper = tickMapper;
-  }
-
-  public @Nonnull Collection<@NotNull TickResponseDto> getTicks(final @Nonnull String symbolName) {
-    return this.getTickRepository().findBySymbol_NameOrderByTimestampAsc(symbolName).stream().map(this.getTickMapper()::toDto).toList();
-  }
-
-  public @Nonnull TickResponseDto addTick(final @Nonnull TickCreateDto tickCreateDto, final @Nonnull String symbolName) {
-    final Symbol symbol = this.getSymbolRepository().findFirstByNameOrderByNameAsc(symbolName)
-        .orElseThrow(() -> new SymbolNotFoundException("Symbol not found"));
-    final Tick tick = this.getTickMapper().toEntity(tickCreateDto);
-    tick.setSymbol(symbol);
-    final Tick saved = this.getTickRepository().save(tick);
-    return this.getTickMapper().toDto(saved);
-  }
+  @Nonnull
+  TickResponseDto addTick(@Nonnull TickCreateDto tickCreateDto, @Nonnull String symbolName);
 }
