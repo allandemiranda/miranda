ea817cab42b954036fd6d297c05d20ae04d177c7
Allan de Miranda Silva <allandemiranda@gmail.com>
2024-04-29 13:53:37 +0200

    b1

--- src/main/java/lu/forex/system/controllers/TickController.java
+++ src/main/java/lu/forex/system/controllers/TickController.java
@@ -1,9 +1,5 @@
 package lu.forex.system.controllers;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import java.util.Collection;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -11,19 +7,11 @@ import lu.forex.system.dtos.TickCreateDto;
 import lu.forex.system.dtos.TickResponseDto;
 import lu.forex.system.services.TickService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/ticks")
 @Getter(AccessLevel.PRIVATE)
-public class TickController {
+public class TickController implements lu.forex.system.operations.TickOperations {
 
   private final TickService tickService;
 
@@ -32,16 +20,13 @@ public class TickController {
     this.tickService = tickService;
   }
 
-  @GetMapping("/{symbolName}")
-  @ResponseStatus(HttpStatus.OK)
-  public Collection<TickResponseDto> getTicksBySymbolName(@PathVariable @NotNull @NotBlank @Size(max = 6, min = 6) String symbolName) {
+  @Override
+  public Collection<TickResponseDto> getTicksBySymbolName(final String symbolName) {
     return this.getTickService().getTicks(symbolName);
   }
 
-  @PostMapping("/{symbolName}")
-  @ResponseStatus(HttpStatus.CREATED)
-  public TickResponseDto addTick(@RequestBody @NotNull @Valid TickCreateDto tickCreateDto,
-      @PathVariable @NotNull @NotBlank @Size(max = 6, min = 6) String symbolName) {
+  @Override
+  public TickResponseDto addTick(final TickCreateDto tickCreateDto, final String symbolName) {
     return this.getTickService().addTick(tickCreateDto, symbolName);
   }
 }
---
+++ src/main/java/lu/forex/system/operations/TickOperations.java
@@ -0,0 +1,28 @@
+package lu.forex.system.operations;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.Collection;
+import lu.forex.system.dtos.TickCreateDto;
+import lu.forex.system.dtos.TickResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@RequestMapping("/ticks")
+public interface TickOperations {
+
+  @GetMapping("/{symbolName}")
+  @ResponseStatus(HttpStatus.OK)
+  Collection<TickResponseDto> getTicksBySymbolName(final @PathVariable @NotBlank @Size(max = 6, min = 6) String symbolName);
+
+  @PostMapping("/{symbolName}")
+  @ResponseStatus(HttpStatus.CREATED)
+  TickResponseDto addTick(final @RequestBody @Valid TickCreateDto tickCreateDto,
+      final @PathVariable @NotBlank @Size(max = 6, min = 6) String symbolName);
+}
