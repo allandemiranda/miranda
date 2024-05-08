cc109cd81748fd78f31102495ddcfcbb99a66560
Allan de Miranda Silva <allandemiranda@gmail.com>
2024-05-08 16:02:06 +0200

    b1

--- src/main/java/lu/forex/system/mappers/CandlestickMapper.java
+++ src/main/java/lu/forex/system/mappers/CandlestickMapper.java
@@ -1,32 +1,13 @@
 package lu.forex.system.mappers;
 
-import lu.forex.system.dtos.CandlestickCreateDto;
 import lu.forex.system.dtos.CandlestickResponseDto;
-import lu.forex.system.dtos.CandlestickUpdateDto;
 import lu.forex.system.entities.Candlestick;
-import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants.ComponentModel;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = ComponentModel.SPRING)
 public interface CandlestickMapper {
 
-  @Mapping(target = "high", source = "candlestickCreateDto.price")
-  @Mapping(target = "low", source = "candlestickCreateDto.price")
-  @Mapping(target = "open", source = "candlestickCreateDto.price")
-  @Mapping(target = "close", source = "candlestickCreateDto.price")
-  Candlestick toEntity(CandlestickCreateDto candlestickCreateDto);
-
-
-  Candlestick toEntity(CandlestickUpdateDto candlestickUpdateDto);
-
   CandlestickResponseDto toDto(Candlestick candlestick);
-
-  @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-  Candlestick partialUpdate(
-      CandlestickUpdateDto candlestickUpdateDto, @MappingTarget Candlestick candlestick);
 }
