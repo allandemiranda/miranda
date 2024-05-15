a7170f8a4ffbda109461aee01d57b892d021ac01
Allan de Miranda Silva <allandemiranda@gmail.com>
2024-05-15 08:46:32 +0200

    b1

---
+++ src/test/java/lu/forex/system/validators/SymbolResponseDtoValidatorTest.java
@@ -0,0 +1,69 @@
+package lu.forex.system.validators;
+
+import jakarta.validation.ConstraintValidatorContext;
+import jakarta.validation.ConstraintValidatorContext.ConstraintViolationBuilder;
+import jakarta.validation.ConstraintValidatorContext.ConstraintViolationBuilder.NodeBuilderCustomizableContext;
+import java.util.Arrays;
+import lu.forex.system.annotations.SymbolCurrencyRepresentation;
+import lu.forex.system.dtos.SymbolResponseDto;
+import lu.forex.system.enums.Currency;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class SymbolResponseDtoValidatorTest {
+
+  @Mock
+  private ConstraintValidatorContext context;
+
+  @InjectMocks
+  private SymbolResponseDtoValidator validator;
+
+  @Mock
+  private SymbolCurrencyRepresentation constraintAnnotation;
+
+  @Mock
+  private ConstraintViolationBuilder constraintViolationBuilder;
+
+  @Mock
+  private NodeBuilderCustomizableContext nodeBuilderCustomizableContext;
+
+  @BeforeEach
+  void init() {
+    Mockito.when(constraintAnnotation.message()).thenReturn("message");
+    Mockito.when(context.buildConstraintViolationWithTemplate(Mockito.anyString())).thenReturn(constraintViolationBuilder);
+    Mockito.when(constraintViolationBuilder.addPropertyNode(Mockito.anyString())).thenReturn(nodeBuilderCustomizableContext);
+  }
+
+  @ParameterizedTest
+  @EnumSource(Currency.class)
+  void testSymbolResponseDtoValidatorWhenCurrencyIsDiffIsValid(Currency currency) {
+    //given
+    final var symbol = Mockito.mock(SymbolResponseDto.class);
+    //when
+    Mockito.when(symbol.currencyBase()).thenReturn(currency);
+    Mockito.when(symbol.currencyQuote()).thenReturn(Arrays.stream(Currency.values()).filter(c -> !currency.equals(c)).findFirst().orElse(null));
+    //then
+    Assertions.assertTrue(validator.isValid(symbol, context));
+  }
+
+  @ParameterizedTest
+  @EnumSource(Currency.class)
+  void testSymbolResponseDtoValidatorWhenCurrencyIsEqualIsInvalid(Currency currency) {
+    //given
+    final var symbol = Mockito.mock(SymbolResponseDto.class);
+    //when
+    Mockito.when(symbol.currencyBase()).thenReturn(currency);
+    Mockito.when(symbol.currencyQuote()).thenReturn(currency);
+    //then
+    Assertions.assertFalse(validator.isValid(symbol, context));
+    Mockito.verify(constraintViolationBuilder).addPropertyNode("currencyBase");
+  }
+}
