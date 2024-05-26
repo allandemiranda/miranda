95ccd092aa413dc5be28260f8cda14d3b52d8db0
Allan de Miranda <allandemiranda@gmail.com>
2024-05-26 14:33:45 +0200

    d1

---
+++ miranda-api/src/main/java/lu/forex/system/enums/CandlestickApply.java
@@ -0,0 +1,30 @@
+package lu.forex.system.enums;
+
+import jakarta.validation.constraints.NotNull;
+import java.util.stream.Stream;
+import lu.forex.system.entities.Candlestick;
+import lu.forex.system.utils.MathUtils;
+
+public enum CandlestickApply {
+  //@formatter:off
+  CLOSE,
+  OPEN,
+  HIGH,
+  LOW,
+  MEDIAN_PRICE,
+  TYPICAL_PRICE;
+  //@formatter:on
+
+  public double price(final @NotNull Candlestick candlestick) {
+    return switch (this) {
+      case CLOSE -> candlestick.getClose();
+      case OPEN -> candlestick.getOpen();
+      case HIGH -> candlestick.getHigh();
+      case LOW -> candlestick.getLow();
+      case MEDIAN_PRICE -> MathUtils.getDivision(MathUtils.getSum(Stream.of(candlestick.getHigh(), candlestick.getLow()).toList()), 2);
+      case TYPICAL_PRICE ->
+          MathUtils.getDivision(MathUtils.getSum(Stream.of(candlestick.getHigh(), candlestick.getLow(), candlestick.getClose()).toList()), 3);
+    };
+  }
+
+}
