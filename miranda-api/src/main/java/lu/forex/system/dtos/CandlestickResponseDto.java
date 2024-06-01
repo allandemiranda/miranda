247d27ec98b04eb8214ada820e9cb589047ec3f7
Allan de Miranda <allandemiranda@gmail.com>
2024-06-01 19:51:05 +0200

    d1

--- miranda-api/src/main/java/lu/forex/system/dtos/CandlestickResponseDto.java
+++ miranda-api/src/main/java/lu/forex/system/dtos/CandlestickResponseDto.java
@@ -1,5 +1,6 @@
 package lu.forex.system.dtos;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Map;
 import java.util.Set;
@@ -12,6 +13,9 @@ import lu.forex.system.enums.Indicator;
 /**
  * DTO for {@link lu.forex.system.entities.Candlestick}
  */
-public record CandlestickResponseDto(CandlestickHead head, CandlestickBody body, Set<MovingAverage> movingAverages, Map<Indicator, TechnicalIndicator> indicators) implements
-    Serializable {
-  }
+public record CandlestickResponseDto(CandlestickHead head, CandlestickBody body, Set<MovingAverage> movingAverages,
+                                     Map<Indicator, TechnicalIndicator> indicators) implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = 7172155089598071933L;
+}
