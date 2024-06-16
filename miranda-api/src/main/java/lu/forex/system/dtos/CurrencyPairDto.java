d44186eb508b045ee7a6e7275e8da8b5b8251b89
Allan de Miranda <allandemiranda@gmail.com>
2024-06-16 14:35:00 +0200

    new1

--- miranda-api/src/main/java/lu/forex/system/dtos/CurrencyPairDto.java
+++ miranda-api/src/main/java/lu/forex/system/dtos/CurrencyPairDto.java
@@ -10,8 +10,8 @@ import lu.forex.system.enums.Currency;
 /**
  * DTO for {@link lu.forex.system.entities.CurrencyPair}
  */
-public record CurrencyPairDto(@NotNull Currency base, @NotNull Currency quote,
-                              @NotNull @Size(min = 6, max = 6) @NotBlank String name, @NotNull @NotBlank String description) implements Serializable {
+public record CurrencyPairDto(@NotNull Currency base, @NotNull Currency quote, @NotNull @Size(min = 6, max = 6) @NotBlank String name,
+                              @NotNull @NotBlank String description) implements Serializable {
 
   @Serial
   private static final long serialVersionUID = -9101405847976389731L;
