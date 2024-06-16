a165f8b0a11ae4d1155d2779d828de6ac4d7f1af
Allan de Miranda <allandemiranda@gmail.com>
2024-06-16 14:32:55 +0200

    new1

--- miranda-api/src/main/java/lu/forex/system/dtos/SwapDto.java
+++ miranda-api/src/main/java/lu/forex/system/dtos/SwapDto.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
  * DTO for {@link lu.forex.system.entities.Swap}
  */
 public record SwapDto(double percentageLong, double percentageShort) implements Serializable {
+
   @Serial
   private static final long serialVersionUID = -8459675499045285992L;
 }
