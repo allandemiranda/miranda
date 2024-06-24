5802c3b04b1cd52c40021da9555f572e727fa9e1
Allan de Miranda <allandemiranda@gmail.com>
2024-06-24 17:24:15 +0200

    new2

--- miranda-api/src/main/java/lu/forex/system/providers/TradeProvider.java
+++ miranda-api/src/main/java/lu/forex/system/providers/TradeProvider.java
@@ -8,7 +8,6 @@ import java.util.AbstractMap.SimpleEntry;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
