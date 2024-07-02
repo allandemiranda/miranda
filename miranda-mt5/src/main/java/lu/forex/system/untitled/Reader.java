a63b06f5a08af25e41eb18e3d3a9401cfec8d4e0
Allan de Miranda <allandemiranda@gmail.com>
2024-07-02 15:30:22 +0200

    newB1

--- miranda-mt5/src/main/java/lu/forex/system/untitled/Reader.java
+++ miranda-mt5/src/main/java/lu/forex/system/untitled/Reader.java
@@ -11,7 +11,6 @@ import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -39,7 +38,7 @@ public class Reader {
   public void start(final String symbol) {
     final String root = "C:\\Users\\AllanDeMirandaSilva\\Downloads\\processing\\";
     final var fileName = root + symbol + CSV;
-    log.info("Reading file {}", fileName);
+    log.info("[{}] Reading file {}",LocalDateTime.now(),fileName);
     final var inputFile = new File(fileName);
     final var bidH = new AtomicReference<>(0D);
     final var askH = new AtomicReference<>(0D);
@@ -52,10 +51,7 @@ public class Reader {
     var lineNow = new AtomicLong(1);
     var lastPercentage = new AtomicLong(-1);
 
-    var lastTickSent = new AtomicReference<>(LocalDateTime.now());
-    var lastTickSentDiff = new AtomicLong(0);
-
-    log.info("Starting...");
+    log.info("[{}] Starting...", LocalDateTime.now());
     try (final var fileReader = new FileReader(inputFile); final var csvParser = CSVFormat.TDF.builder().build().parse(fileReader)) {
       StreamSupport.stream(csvParser.spliterator(), false).skip(1).map(this::getDataTick).forEachOrdered(tick -> {
         final long percentage = lineNow.addAndGet(1) * 100L / numLines.get();
@@ -73,17 +69,12 @@ public class Reader {
           tick.setBid(bidH.get());
           tick.setAsk(askH.get());
           this.sent(symbol, tick);
-          final var diffTime = ChronoUnit.SECONDS.between(lastTickSent.get(), LocalDateTime.now());
-          if(diffTime != lastTickSentDiff.get()) {
-            log.info("TimeDiff sent: {}", diffTime);
-            lastTickSentDiff.set(diffTime);
-            lastTickSent.set(LocalDateTime.now());
-          }
           lastUpdate.set(tick.getTime());
         }
       });
     }
 
+    log.info("[{}] Writing balance...", LocalDateTime.now());
     try (final FileWriter fileWriter = new FileWriter(new File(new File(root), "balance.csv"));
         final CSVWriter csvWriter = new CSVWriter(fileWriter)) {
       this.getBalanceHistoric().stream().map(entry -> new String[]{entry.getKey().toString(), String.valueOf(entry.getValue())})
@@ -110,29 +101,37 @@ public class Reader {
           .header("User-Agent", "insomnia/9.0.0")
           .method("POST", HttpRequest.BodyPublishers.ofString(body))
           .build();
-      log.warn(tick.toString());
       final var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-      if (response.statusCode() == 500) {
-        log.error(response.body());
-      } else if (response.statusCode() == 400) {
-        log.warn(response.body());
-      } else if (response.statusCode() == 201) {
-        if (!response.body().isEmpty()) {
-          log.info("--> Received order: {}", response.body());
-          final Collection<Order> orders = Arrays.stream(response.body().split(","))
-              .map(order -> order.split(" "))
-              .map(order -> Order.builder().tp(Integer.parseInt(order[3])).sl(Integer.parseInt(order[4])).openTick(tick).closeTick(tick)
-                  .type(order[2].equals("BUY") ? Type.BUY : Type.SELL).status(Status.OPEN).build())
-              .toList();
-          log.info("--> Created orders:");
-          orders.forEach(order -> log.info("{}", order));
-          this.getOrderRepository().addAll(orders);
-          this.getBalanceHistoric().add(new SimpleEntry<>(tick.getTime(), this.calculateBalance(tick)));
-          log.info("--> Tmp balance: {}", this.getBalanceHistoric().getLast().getValue());
+      switch (response.statusCode()) {
+        case 200: {
+          log.warn("[{}] Return 200 not expected", LocalDateTime.now());
+          break;
         }
-      } else {
-        log.error("Code: {}", response.statusCode());
-        Arrays.stream(response.body().split("\r\n\tat")).forEachOrdered(log::error);
+        case 201: {
+          if (!response.body().isEmpty()) {
+            log.info("--> Received order: {}", response.body());
+            final Collection<Order> orders = Arrays.stream(response.body().split(","))
+                .map(order -> order.split(" "))
+                .map(order -> Order.builder().tp(Integer.parseInt(order[3])).sl(Integer.parseInt(order[4])).openTick(tick).closeTick(tick)
+                    .type(order[2].equals("BUY") ? Type.BUY : Type.SELL).status(Status.OPEN).build())
+                .toList();
+            log.info("--> Created orders:");
+            orders.forEach(order -> log.info("{}", order));
+            this.getOrderRepository().addAll(orders);
+            this.getBalanceHistoric().add(new SimpleEntry<>(tick.getTime(), this.calculateBalance(tick)));
+            log.info("--> Tmp balance: {}", this.getBalanceHistoric().getLast().getValue());
+          }
+          break;
+        }
+        case 400: {
+          log.warn("[{}] Return 400 not expected: {}", LocalDateTime.now(), response.body());
+          break;
+        }
+        case 500: {
+          log.error("[{}] Return 500 not expected: {}", LocalDateTime.now(), response.body());
+          break;
+        }
+        default: log.error("[{}] Return unknown error code {}: {}", response.statusCode(), LocalDateTime.now(), response.body());
       }
     }
   }
