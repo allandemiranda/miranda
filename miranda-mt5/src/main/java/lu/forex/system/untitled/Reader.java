1b40c5b8d93c970fd61dd790b7f1dca4d9a7b66d
Allan de Miranda <allandemiranda@gmail.com>
2024-07-02 15:19:35 +0200

    newB1

--- miranda-mt5/src/main/java/lu/forex/system/untitled/Reader.java
+++ miranda-mt5/src/main/java/lu/forex/system/untitled/Reader.java
@@ -11,6 +11,7 @@ import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,7 +31,7 @@ import org.jetbrains.annotations.NotNull;
 @Getter
 public class Reader {
 
-  private static final String CSV = "_202310020000_202403292258_3meses.csv";
+  private static final String CSV = " - Copia (2e5m).csv";
   private final Collection<Order> orderRepository = new ArrayList<>();
   private final List<SimpleEntry<LocalDateTime, Double>> balanceHistoric = new ArrayList<>();
 
@@ -38,48 +39,56 @@ public class Reader {
   public void start(final String symbol) {
     final String root = "C:\\Users\\AllanDeMirandaSilva\\Downloads\\processing\\";
     final var fileName = root + symbol + CSV;
-      log.info("Reading file {}", fileName);
-      final var inputFile = new File(fileName);
-      final var bidH = new AtomicReference<>(0D);
-      final var askH = new AtomicReference<>(0D);
-      final var lastUpdate = new AtomicReference<>(LocalDateTime.MIN);
+    log.info("Reading file {}", fileName);
+    final var inputFile = new File(fileName);
+    final var bidH = new AtomicReference<>(0D);
+    final var askH = new AtomicReference<>(0D);
+    final var lastUpdate = new AtomicReference<>(LocalDateTime.MIN);
 
-      var numLines = new AtomicLong(0L);
-      try(final var lines = Files.lines(inputFile.toPath())) {
-        numLines.set(lines.count() - 1L);
-      }
-      var lineNow = new AtomicLong(1);
-      var lastPercentage = new AtomicLong(-1);
+    var numLines = new AtomicLong(0L);
+    try (final var lines = Files.lines(inputFile.toPath())) {
+      numLines.set(lines.count() - 1L);
+    }
+    var lineNow = new AtomicLong(1);
+    var lastPercentage = new AtomicLong(-1);
 
-      log.info("Starting...");
-      try (final var fileReader = new FileReader(inputFile); final var csvParser = CSVFormat.TDF.builder().build().parse(fileReader)) {
-        StreamSupport.stream(csvParser.spliterator(), false).skip(1).map(this::getDataTick).forEachOrdered(tick -> {
-          final long percentage = lineNow.addAndGet(1) * 100L / numLines.get();
-          if(percentage != lastPercentage.get()) {
-            log.info("[{}] {}% read", LocalDateTime.now(), percentage);
-            lastPercentage.set(percentage);
+    var lastTickSent = new AtomicReference<>(LocalDateTime.now());
+    var lastTickSentDiff = new AtomicLong(0);
+
+    log.info("Starting...");
+    try (final var fileReader = new FileReader(inputFile); final var csvParser = CSVFormat.TDF.builder().build().parse(fileReader)) {
+      StreamSupport.stream(csvParser.spliterator(), false).skip(1).map(this::getDataTick).forEachOrdered(tick -> {
+        final long percentage = lineNow.addAndGet(1) * 100L / numLines.get();
+        if (percentage != lastPercentage.get()) {
+          log.info("[{}] {}% read", LocalDateTime.now(), percentage);
+          lastPercentage.set(percentage);
+        }
+        if (tick.getBid() > 0D) {
+          bidH.set(tick.getBid());
+        }
+        if (tick.getAsk() > 0D) {
+          askH.set(tick.getAsk());
+        }
+        if ((bidH.get() > 0D) && (askH.get() > 0D) && tick.getTime().isAfter(lastUpdate.get())) {
+          tick.setBid(bidH.get());
+          tick.setAsk(askH.get());
+          this.sent(symbol, tick);
+          final var diffTime = ChronoUnit.SECONDS.between(lastTickSent.get(), LocalDateTime.now());
+          if(diffTime != lastTickSentDiff.get()) {
+            log.info("TimeDiff sent: {}", diffTime);
+            lastTickSentDiff.set(diffTime);
+            lastTickSent.set(LocalDateTime.now());
           }
-          if (tick.getBid() != bidH.get() || tick.getAsk() != askH.get()) {
-            if (tick.getBid() > 0D) {
-              bidH.set(tick.getBid());
-            }
-            if (tick.getAsk() > 0D) {
-              askH.set(tick.getAsk());
-            }
-            if ((bidH.get() > 0D) && (askH.get() > 0D) && tick.getTime().isAfter(lastUpdate.get())) {
-              try (final var httpClient = HttpClient.newHttpClient()) {
-                this.sent(httpClient, symbol, tick);
-              }
-              lastUpdate.set(tick.getTime());
-            }
-          }
-        });
-      }
+          lastUpdate.set(tick.getTime());
+        }
+      });
+    }
 
-      try(final FileWriter fileWriter = new FileWriter(new File(new File(root), "balance.csv"));
-          final CSVWriter csvWriter = new CSVWriter(fileWriter)){
-        this.getBalanceHistoric().stream().map(entry -> new String[]{entry.getKey().toString(), String.valueOf(entry.getValue())}).forEachOrdered(csvWriter::writeNext);
-      }
+    try (final FileWriter fileWriter = new FileWriter(new File(new File(root), "balance.csv"));
+        final CSVWriter csvWriter = new CSVWriter(fileWriter)) {
+      this.getBalanceHistoric().stream().map(entry -> new String[]{entry.getKey().toString(), String.valueOf(entry.getValue())})
+          .forEachOrdered(csvWriter::writeNext);
+    }
   }
 
   private @NotNull Tick getDataTick(final @NotNull CSVRecord csvRecord) {
@@ -93,34 +102,38 @@ public class Reader {
   }
 
   @SneakyThrows
-  private synchronized void sent(final @NotNull HttpClient httpClient, final String symbol, @NotNull final Tick tick) {
-    final var body = "{\n  \"timestamp\": \"" + tick.getTime().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) + "\",\n  \"bid\": " + tick.getBid() + ",\n  \"ask\": " + tick.getAsk() + "\n}";
-    final var request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/api/v1/ticks/" + symbol))
-        .header("Content-Type", "application/json")
-        .header("User-Agent", "insomnia/9.0.0")
-        .method("POST", HttpRequest.BodyPublishers.ofString(body))
-        .build();
-    final var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-    if (response.statusCode() == 500) {
-      log.error(response.body());
-    } else if(response.statusCode() == 400) {
-      log.warn(response.body());
-    } else if(response.statusCode() == 201) {
-      if (response.body().isEmpty()) {
-        log.info("--> Received order: {}", response.body());
-        final Collection<Order> orders = Arrays.stream(response.body().split(","))
-            .map(order -> order.split(" "))
-            .map(order -> Order.builder().tp(Integer.parseInt(order[3])).sl(Integer.parseInt(order[4])).openTick(tick).closeTick(tick).type(order[2].equals("BUY") ? Type.BUY : Type.SELL).status(Status.OPEN).build())
-            .toList();
-        log.info("--> Created orders:");
-        orders.forEach(order -> log.info("{}", order));
-        this.getOrderRepository().addAll(orders);
-        this.getBalanceHistoric().add(new SimpleEntry<>(tick.getTime(), this.calculateBalance(tick)));
-        log.info("--> Tmp balance: {}", this.getBalanceHistoric().getLast().getValue());
+  private void sent(final String symbol, @NotNull final Tick tick) {
+    try (final var httpClient = HttpClient.newHttpClient()) {
+      final var body = "{\n  \"timestamp\": \"" + tick.getTime().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) + "\",\n  \"bid\": " + tick.getBid() + ",\n  \"ask\": " + tick.getAsk() + "\n}";
+      final var request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/api/v1/ticks/" + symbol))
+          .header("Content-Type", "application/json")
+          .header("User-Agent", "insomnia/9.0.0")
+          .method("POST", HttpRequest.BodyPublishers.ofString(body))
+          .build();
+      log.warn(tick.toString());
+      final var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+      if (response.statusCode() == 500) {
+        log.error(response.body());
+      } else if (response.statusCode() == 400) {
+        log.warn(response.body());
+      } else if (response.statusCode() == 201) {
+        if (!response.body().isEmpty()) {
+          log.info("--> Received order: {}", response.body());
+          final Collection<Order> orders = Arrays.stream(response.body().split(","))
+              .map(order -> order.split(" "))
+              .map(order -> Order.builder().tp(Integer.parseInt(order[3])).sl(Integer.parseInt(order[4])).openTick(tick).closeTick(tick)
+                  .type(order[2].equals("BUY") ? Type.BUY : Type.SELL).status(Status.OPEN).build())
+              .toList();
+          log.info("--> Created orders:");
+          orders.forEach(order -> log.info("{}", order));
+          this.getOrderRepository().addAll(orders);
+          this.getBalanceHistoric().add(new SimpleEntry<>(tick.getTime(), this.calculateBalance(tick)));
+          log.info("--> Tmp balance: {}", this.getBalanceHistoric().getLast().getValue());
+        }
+      } else {
+        log.error("Code: {}", response.statusCode());
+        Arrays.stream(response.body().split("\r\n\tat")).forEachOrdered(log::error);
       }
-    } else {
-      log.error("Code: {}", response.statusCode());
-      Arrays.stream(response.body().split("\r\n\tat")).forEachOrdered(log::error);
     }
   }
 
