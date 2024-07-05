510c73d9c0f5b57023fd11269e2e5d75593a5d0f
Allan de Miranda <allandemiranda@gmail.com>
2024-07-05 17:57:34 +0200

    newB1

--- miranda-api/src/main/java/lu/forex/system/controllers/TradeController.java
+++ miranda-api/src/main/java/lu/forex/system/controllers/TradeController.java
@@ -4,10 +4,12 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.DayOfWeek;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -30,6 +32,7 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cglib.core.Local;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -78,91 +81,131 @@ public class TradeController implements TradeOperation {
         });
       });
 
+      final Map<DayOfWeek, Map<Pair<LocalTime, LocalTime>, List<TradeDto>>> mapTradesAll = trades.stream().collect(Collectors.groupingBy(TradeDto::slotWeek, Collectors.groupingBy(tradeDto -> Pair.of(tradeDto.slotStart(), tradeDto.slotEnd()))));
+      final Sheet sheetMapTradesAll = workbook.createSheet("map_trades_ALL");
+      final List<Pair<LocalTime, LocalTime>> timesColumn = mapTradesAll.values().stream().flatMap(pairListMap -> pairListMap.keySet().stream()).distinct().sorted(Entry.comparingByKey()).toList();
+      final List<DayOfWeek> weeksHeader = mapTradesAll.keySet().stream().sorted().toList();
+      final long[][] matrixSL = new long[timesColumn.size()][weeksHeader.size()];
+      final long[][] matrixTP = new long[timesColumn.size()][weeksHeader.size()];
+      final long[][] matrixCLOSE = new long[timesColumn.size()][weeksHeader.size()];
+      for (int i=0; i<matrixSL.length; ++i){
+        for(int j=0; j<matrixSL[i].length; ++j){
+          final long tpSum = mapTradesAll.get(weeksHeader.get(j)).get(timesColumn.get(i)).stream().mapToLong(tradeDto -> tradeDto.orders().stream().filter(order -> order.orderStatus().equals(OrderStatus.TAKE_PROFIT)).count()).sum();
+          final long slSun = mapTradesAll.get(weeksHeader.get(j)).get(timesColumn.get(i)).stream().mapToLong(tradeDto -> tradeDto.orders().stream().filter(order -> order.orderStatus().equals(OrderStatus.STOP_LOSS)).count()).sum();
+          final long closeSun = mapTradesAll.get(weeksHeader.get(j)).get(timesColumn.get(i)).stream().mapToLong(tradeDto -> tradeDto.orders().stream().filter(order -> !order.orderStatus().equals(OrderStatus.OPEN)).count()).sum();
+          matrixTP[i][j] = tpSum;
+          matrixSL[i][j] = slSun;
+          matrixCLOSE[i][j] = closeSun;
+        }
+      }
+      final List<long[][]> matrixs = List.of(matrixTP, matrixSL, matrixCLOSE);
+      for(int k=0; k<3; ++k){
+        final int rowNumHeader = (timesColumn.size() + 2) * k;
+        final Row headerK = sheetMapTradesAll.createRow(rowNumHeader);
+        headerK.createCell(0).setCellValue("Start");
+        headerK.createCell(1).setCellValue("End");
+        for(int j=2; j<weeksHeader.size()+2; ++j){
+          headerK.createCell(j).setCellValue(weeksHeader.get(j-2).toString());
+        }
 
-      mappingTrades.forEach((timeFrame, entryValue) -> {
+        for(int j=0; j<timesColumn.size(); ++j){
+          final LocalTime start = timesColumn.get(j).getKey();
+          final LocalTime end = timesColumn.get(j).getValue();
+          sheetMapTradesAll.createRow((j+1)+rowNumHeader).createCell(0).setCellValue(start.toString());
+          sheetMapTradesAll.getRow((j+1)+rowNumHeader).createCell(1).setCellValue(end.toString());
+        }
 
-        final Sheet sheetTrades = workbook.createSheet("trades_".concat(timeFrame.name()));
-        final Row headerTrades = sheetTrades.createRow(0);
-        final String[] headerTradesNames = new String[]{"ID", "Stop Loss", "Take Profit", "Spread Max", "Slot Week", "Slot Time Start",
-            "Slot Time End", "Balance", "Number of Stop Loss", "Number of Take Profit", "Number of Close Orders", "Number of Orders", "Is Activate"};
-        IntStream.range(0, headerTradesNames.length).forEach(i -> {
-          final var cell = headerTrades.createCell(i);
-          cell.setCellValue(headerTradesNames[i]);
-        });
+        for (int i=0; i<matrixSL.length; ++i){
+          for(int j=0; j<matrixSL[i].length; ++j){
+            sheetMapTradesAll.getRow((i+1)+rowNumHeader).createCell(j+2).setCellValue(matrixs.get(k)[i][j]);
+          }
+        }
+      }
 
-        final Object[][] tmpTradesDto = entryValue.entrySet().stream().flatMap(
-            tripleMapEntry -> tripleMapEntry.getValue().entrySet().stream()
-                .flatMap(dayOfWeekListEntry -> dayOfWeekListEntry.getValue().values().stream())).map(
-            tradeDto -> new Object[]{tradeDto.id().toString(), tradeDto.stopLoss(), tradeDto.takeProfit(), tradeDto.spreadMax(),
-                tradeDto.slotWeek().toString(), tradeDto.slotStart().toString(), tradeDto.slotEnd().toString(), tradeDto.balance(),
-                tradeDto.orders().stream().filter(orderDto -> OrderStatus.STOP_LOSS.equals(orderDto.orderStatus())).count(),
-                tradeDto.orders().stream().filter(orderDto -> OrderStatus.TAKE_PROFIT.equals(orderDto.orderStatus())).count(),
-                tradeDto.orders().stream().filter(orderDto -> !OrderStatus.OPEN.equals(orderDto.orderStatus())).count(), tradeDto.orders().size(),
-                String.valueOf(tradeDto.isActivate())}).toArray(Object[][]::new);
-        IntStream.range(1, tmpTradesDto.length + 1).forEach(i -> {
-          final Row tradesRowData = sheetTrades.createRow(i);
-          IntStream.range(0, tmpTradesDto[i - 1].length).forEach(j -> {
-            final var cell = tradesRowData.createCell(j);
-            switch (tmpTradesDto[i - 1][j]) {
-              case String text -> cell.setCellValue(text);
-              case Integer integer -> cell.setCellValue(integer);
-              case Double doubles -> cell.setCellValue(doubles);
-              case Long longs -> cell.setCellValue(longs);
-              default -> throw new IllegalStateException("Unexpected value: " + tmpTradesDto[i - 1][j]);
-            }
-          });
-        });
+//      mappingTrades.forEach((timeFrame, entryValue) -> {
+//
+//        final Sheet sheetTrades = workbook.createSheet("trades_".concat(timeFrame.name()));
+//        final Row headerTrades = sheetTrades.createRow(0);
+//        final String[] headerTradesNames = new String[]{"ID", "Stop Loss", "Take Profit", "Spread Max", "Slot Week", "Slot Time Start",
+//            "Slot Time End", "Balance", "Number of Stop Loss", "Number of Take Profit", "Number of Close Orders", "Number of Orders", "Is Activate"};
+//        IntStream.range(0, headerTradesNames.length).forEach(i -> {
+//          final var cell = headerTrades.createCell(i);
+//          cell.setCellValue(headerTradesNames[i]);
+//        });
+//
+//        final Object[][] tmpTradesDto = entryValue.entrySet().stream().flatMap(
+//            tripleMapEntry -> tripleMapEntry.getValue().entrySet().stream()
+//                .flatMap(dayOfWeekListEntry -> dayOfWeekListEntry.getValue().values().stream())).map(
+//            tradeDto -> new Object[]{tradeDto.id().toString(), tradeDto.stopLoss(), tradeDto.takeProfit(), tradeDto.spreadMax(),
+//                tradeDto.slotWeek().toString(), tradeDto.slotStart().toString(), tradeDto.slotEnd().toString(), tradeDto.balance(),
+//                tradeDto.orders().stream().filter(orderDto -> OrderStatus.STOP_LOSS.equals(orderDto.orderStatus())).count(),
+//                tradeDto.orders().stream().filter(orderDto -> OrderStatus.TAKE_PROFIT.equals(orderDto.orderStatus())).count(),
+//                tradeDto.orders().stream().filter(orderDto -> !OrderStatus.OPEN.equals(orderDto.orderStatus())).count(), tradeDto.orders().size(),
+//                String.valueOf(tradeDto.isActivate())}).toArray(Object[][]::new);
+//        IntStream.range(1, tmpTradesDto.length + 1).forEach(i -> {
+//          final Row tradesRowData = sheetTrades.createRow(i);
+//          IntStream.range(0, tmpTradesDto[i - 1].length).forEach(j -> {
+//            final var cell = tradesRowData.createCell(j);
+//            switch (tmpTradesDto[i - 1][j]) {
+//              case String text -> cell.setCellValue(text);
+//              case Integer integer -> cell.setCellValue(integer);
+//              case Double doubles -> cell.setCellValue(doubles);
+//              case Long longs -> cell.setCellValue(longs);
+//              default -> throw new IllegalStateException("Unexpected value: " + tmpTradesDto[i - 1][j]);
+//            }
+//          });
+//        });
 
-        entryValue.forEach((tripleSlTpSm, tripleMapEntryValue) -> {
-
-          final String sheetNameBalance = "windows_balance_".concat(tripleSlTpSm.getLeft().toString()).concat("_")
-              .concat(tripleSlTpSm.getMiddle().toString()).concat("_").concat(tripleSlTpSm.getRight().toString()).concat("_")
-              .concat(timeFrame.getName());
-          final Sheet sheetBalance = workbook.createSheet(sheetNameBalance);
-          final String sheetNameWindows = "windows_TP_SL_SM_".concat(tripleSlTpSm.getLeft().toString()).concat("_")
-              .concat(tripleSlTpSm.getMiddle().toString()).concat("_").concat(tripleSlTpSm.getRight().toString()).concat("_")
-              .concat(timeFrame.getName());
-          final Sheet sheetWindows = workbook.createSheet(sheetNameWindows);
-
-          final DayOfWeek[] weeks = tripleMapEntryValue.keySet().toArray(DayOfWeek[]::new);
-          final Row headerBalance = sheetBalance.createRow(0);
-          IntStream.range(0, weeks.length).forEach(i -> {
-            final var cell = headerBalance.createCell(i + 1);
-            cell.setCellValue(weeks[i].toString());
-          });
-          final Row headerWindows = sheetWindows.createRow(0);
-          IntStream.range(0, weeks.length).forEach(i -> {
-            final var cell = headerWindows.createCell(i + 1);
-            cell.setCellValue(weeks[i].toString());
-          });
-
-          final Pair<LocalTime, LocalTime>[] timesPair = tripleMapEntryValue.get(weeks[0]).keySet().stream().sorted(Entry.comparingByKey()).toArray(Pair[]::new);
-          IntStream.range(1, timesPair.length + 1).forEach(i -> {
-            final Row balanceRowData = sheetBalance.createRow(i);
-            IntStream.range(0, weeks.length + 1).forEach(j -> {
-              final var cell = balanceRowData.createCell(j);
-              if (j == 0) {
-                cell.setCellValue(timesPair[i - 1].toString());
-              } else {
-                final var t = tripleMapEntryValue.get(weeks[j - 1]).get(timesPair[i - 1]);
-                cell.setCellValue(t.balance());
-              }
-            });
-          });
-          IntStream.range(1, timesPair.length + 1).forEach(i -> {
-            final Row balanceRowData = sheetWindows.createRow(i);
-            IntStream.range(0, weeks.length + 1).forEach(j -> {
-              final var cell = balanceRowData.createCell(j);
-              if (j == 0) {
-                cell.setCellValue(timesPair[i - 1].toString());
-              } else {
-                final var t = tripleMapEntryValue.get(weeks[j - 1]).get(timesPair[i - 1]);
-                cell.setCellValue(t.balance());
-              }
-            });
-          });
-        });
-      });
+//        entryValue.forEach((tripleSlTpSm, tripleMapEntryValue) -> {
+//
+//          final String sheetNameBalance = "windows_balance_".concat(tripleSlTpSm.getLeft().toString()).concat("_")
+//              .concat(tripleSlTpSm.getMiddle().toString()).concat("_").concat(tripleSlTpSm.getRight().toString()).concat("_")
+//              .concat(timeFrame.getName());
+//          final Sheet sheetBalance = workbook.createSheet(sheetNameBalance);
+//          final String sheetNameWindows = "windows_TP_SL_SM_".concat(tripleSlTpSm.getLeft().toString()).concat("_")
+//              .concat(tripleSlTpSm.getMiddle().toString()).concat("_").concat(tripleSlTpSm.getRight().toString()).concat("_")
+//              .concat(timeFrame.getName());
+//          final Sheet sheetWindows = workbook.createSheet(sheetNameWindows);
+//
+//          final DayOfWeek[] weeks = tripleMapEntryValue.keySet().toArray(DayOfWeek[]::new);
+//          final Row headerBalance = sheetBalance.createRow(0);
+//          IntStream.range(0, weeks.length).forEach(i -> {
+//            final var cell = headerBalance.createCell(i + 1);
+//            cell.setCellValue(weeks[i].toString());
+//          });
+//          final Row headerWindows = sheetWindows.createRow(0);
+//          IntStream.range(0, weeks.length).forEach(i -> {
+//            final var cell = headerWindows.createCell(i + 1);
+//            cell.setCellValue(weeks[i].toString());
+//          });
+//
+//          final Pair<LocalTime, LocalTime>[] timesPair = tripleMapEntryValue.get(weeks[0]).keySet().stream().sorted(Entry.comparingByKey()).toArray(Pair[]::new);
+//          IntStream.range(1, timesPair.length + 1).forEach(i -> {
+//            final Row balanceRowData = sheetBalance.createRow(i);
+//            IntStream.range(0, weeks.length + 1).forEach(j -> {
+//              final var cell = balanceRowData.createCell(j);
+//              if (j == 0) {
+//                cell.setCellValue(timesPair[i - 1].toString());
+//              } else {
+//                final var t = tripleMapEntryValue.get(weeks[j - 1]).get(timesPair[i - 1]);
+//                cell.setCellValue(t.balance());
+//              }
+//            });
+//          });
+//          IntStream.range(1, timesPair.length + 1).forEach(i -> {
+//            final Row balanceRowData = sheetWindows.createRow(i);
+//            IntStream.range(0, weeks.length + 1).forEach(j -> {
+//              final var cell = balanceRowData.createCell(j);
+//              if (j == 0) {
+//                cell.setCellValue(timesPair[i - 1].toString());
+//              } else {
+//                final var t = tripleMapEntryValue.get(weeks[j - 1]).get(timesPair[i - 1]);
+//                cell.setCellValue(t.balance());
+//              }
+//            });
+//          });
+//        });
+//      });
 
       final var root = new File(this.getFilePathInit());
       final var fileXlsx = new File(root, symbolName.concat("_data.xlsx"));
